ff3adfec19c73651ecf1ae04d76922a27ff39571
mitchmertz <m.mertz93@gmail.com>
2024-08-11 23:26:05 +0200

    add setup bash script

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Load environment variables from .env file
+source .env
+
+# Create data directory if it doesn't exist
+mkdir -p ./data
+
+# Set correct permissions
+sudo chown -R $UID:$GID ./data
+sudo chmod -R 755 ./data
+
+# Export environment variables
+export PUID=$UID
+export PGID=$GID
+export TZ="$TZ"
+
+# Run docker-compose
+docker-compose up -d
