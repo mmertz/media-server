52ca248cfa44aa58e542bfcba367062d27078233
mitchmertz <m.mertz93@gmail.com>
2024-08-12 18:53:08 +0200

    .

--- setup.sh
+++ setup.sh
@@ -1,44 +1,11 @@
 #!/bin/bash
 
 source .env
+# Set environment variables
+export PUID=$(id -u)
+export PGID=$(id -g)
+export TZ="${TZ:-UTC}"
 
-generate_api_key() {
-    openssl rand -hex 16
-}
-
-export SONARR_API_KEY="${SONARR_API_KEY:-$(generate_api_key)}"
-export RADARR_API_KEY="${RADARR_API_KEY:-$(generate_api_key)}"
-export PROWLARR_API_KEY="${PROWLARR_API_KEY:-$(generate_api_key)}"
-export BAZARR_API_KEY="${BAZARR_API_KEY:-$(generate_api_key)}"
-
-sed -i "s/^SONARR_API_KEY=.*/SONARR_API_KEY=\"$SONARR_API_KEY\"/" .env
-sed -i "s/^RADARR_API_KEY=.*/RADARR_API_KEY=\"$RADARR_API_KEY\"/" .env
-sed -i "s/^PROWLARR_API_KEY=.*/PROWLARR_API_KEY=\"$PROWLARR_API_KEY\"/" .env
-sed -i "s/^BAZARR_API_KEY=.*/BAZARR_API_KEY=\"$BAZARR_API_KEY\"/" .env
-
+# Restart Docker containers
 docker-compose down
-
-sudo rm -rf ./data
-
-sudo rm -rf ./data
-
-sudo chown -R $UID:$GID ./data
-sudo chmod -R 755 ./data
-
-export PUID=$UID
-export PGID=$GID
-export TZ="$TZ"
-export NZBGEEK_API_KEY="$NZBGEEK_API_KEY"
-
-export SONARR_API_KEY="${SONARR_API_KEY:-$(generate_api_key)}"
-export RADARR_API_KEY="${RADARR_API_KEY:-$(generate_api_key)}"
-export PROWLARR_API_KEY="${PROWLARR_API_KEY:-$(generate_api_key)}"
-export BAZARR_API_KEY="${BAZARR_API_KEY:-$(generate_api_key)}"
-
-sed -i "s/^SONARR_API_KEY=.*/SONARR_API_KEY=\"$SONARR_API_KEY\"/" .env
-sed -i "s/^RADARR_API_KEY=.*/RADARR_API_KEY=\"$RADARR_API_KEY\"/" .env
-sed -i "s/^PROWLARR_API_KEY=.*/PROWLARR_API_KEY=\"$PROWLARR_API_KEY\"/" .env
-sed -i "s/^BAZARR_API_KEY=.*/BAZARR_API_KEY=\"$BAZARR_API_KEY\"/" .env
-
-
 docker-compose up -d --force-recreate
