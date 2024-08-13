ee70b006c4e677694ccb451157248a1a70249966
mitchmertz <m.mertz93@gmail.com>
2024-08-13 14:45:57 +0200

    .

--- setup.sh
+++ setup.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-source .env
-# Set environment variables
-export PUID=$(id -u)
-export PGID=$(id -g)
-export TZ="${TZ:-UTC}"
-
-# Restart Docker containers
 docker-compose down
-docker-compose up -d --force-recreate
+docker-compose up -d --force-recreate --remove-orphans
